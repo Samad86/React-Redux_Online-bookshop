69034774dab429757d453b4f58e1ef5c26aef46a
Samad Kurbanov <mads198644@gmail.com>
2020-08-02 22:04:38 +0300

    Add files via upload

---
+++ src/actions/filter.js
@@ -0,0 +1,9 @@
+export const setSearchQuery = (value) => ({
+  type: "SET_SEARCH_QUERY",
+  payload: value,
+});
+
+export const setFilter = (filter) => ({
+  type: "SET_FILTER",
+  payload: filter,
+});
